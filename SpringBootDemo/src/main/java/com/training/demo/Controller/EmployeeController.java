73d6fceda2e89f1a24e25d5da0c7b18efbb7a38f
sagar <sagar.mahalatkar@capgemini.com>
2019-11-06 14:43:57 +0530

    addingprojectcommitted

---
+++ SpringBootDemo/src/main/java/com/training/demo/Controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.training.demo.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.training.demo.model.Employee;
+
+public class EmployeeController {
+
+	@RequestMapping("/")
+    public List<Employee> getEmployees() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		return employeesList;
+    }
+}
